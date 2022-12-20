2f815e1ccc0da5ad6e9c5312d6ae02f392089815
Suhaib-Arif <115211151+Suhaib-Arif@users.noreply.github.com>
2022-12-20 16:24:54 +0530

    Delete Day-34-Quizzler-app directory

--- Day-34-Quizzler-app/data.py
+++
@@ -1,9 +0,0 @@
-# question_data =
-
-import requests
-
-data=requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
-
-unorganized_question_data=data.json()
-
-question_data=unorganized_question_data["results"]
